544e13b429399ef8b2eb4ce2385355c5380fd0cc
ibrahimgbede <ibrahimgbede@gmail.com>
2026-08-06 08:33:23 +0300

    bash script on Africa

---
+++ bash/variables2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+land="Algeria"
+population="Nigeria"
+
+
+echo "The largest country in Africa by land mass is $land, whilst the most populous country on the continent is $population"
+
+
